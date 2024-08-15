4ecc6576ea01a2b2f504379f32b1d0a94e8ee1db
greallra <greallra@tcd.ie>
2024-08-15 18:06:39 +0100

    udpate

---
+++ index.ts
@@ -0,0 +1,46 @@
+// import { Exchange, User, ParticipantsTeachingLanguage, ParticipantsLearningLanguage } from './types'
+
+export function checkUserIsValidToJoin(
+        exchange,
+        participantsTeachingLanguage,
+        participantsLearningLanguage,
+        userPerforming, 
+        userAdded
+    ) {
+    let isValid = false;
+    let message = '';
+    let joiningUser;
+    let joiningUserIsMe = false;
+
+    if (!exchange || !userPerforming) {
+        message = 'exchange and user Performing are required'
+        return {
+            isValid,
+            message
+        }
+    }
+
+    if (!userAdded) {
+        joiningUser = userPerforming
+        joiningUserIsMe = true
+    } else {
+        joiningUser = userAdded 
+    }
+
+    // Already Joined
+    if (exchange.participantIds.includes(joiningUser.id) || exchange.participantIds.includes(joiningUser.uid)) {
+        message = joiningUserIsMe ? 'You have already joined this exchange': `User ${joiningUser.username} has already joined this exchange`;
+    }
+    // exchange full TL
+    else if (participantsTeachingLanguage.length >= exchange.capacity / 2 && participantsTeachingLanguage[0].teachingLanguageId === joiningUser.teachingLanguageId ) {
+        message = `The Exchange is full for ${exchange.teachingLanguageUnfolded.name} speakers`;
+    }
+    // exchange full LL
+    else if (participantsLearningLanguage.length >= exchange.capacity / 2 && participantsLearningLanguage[0].learningLanguageId === joiningUser.learningLanguageId) {
+        message = `The Exchange is full for ${exchange.learningLanguageUnfolded.name} speakers`;
+    }
+    return {
+        isValid,
+        message
+    }
+}
---
+++ types.ts
@@ -0,0 +1,13 @@
+export interface Exchange {
+    name: string
+}
+export interface User {
+    username: string
+}
+export interface ParticipantsTeachingLanguage {
+    // username: string
+}
+export interface ParticipantsLearningLanguage {
+    // username: string
+}
+
